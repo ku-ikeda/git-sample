f4f915cd6b183e99b7dc5fa00fb73125020dbe56
yuta.ikeda <yuta.ikeda@rakus.co.jp>
2018-11-19 11:27:16 +0900

    Carクラス編集

--- src/main/java/jp/co/sample/Class/Car.java
+++ src/main/java/jp/co/sample/Class/Car.java
@@ -2,4 +2,38 @@ package jp.co.sample.Class;
 
 public class Car {
 
+	private int speed;
+
+	private String bodyColor;
+
+	public Car() {
+	}
+
+	public Car(int speed, String bodyColor) {
+		super();
+		this.speed = speed;
+		this.bodyColor = bodyColor;
+	}
+
+	@Override
+	public String toString() {
+		return "Car [speed=" + speed + ", bodyColor=" + bodyColor + "]";
+	}
+
+	public int getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(int speed) {
+		this.speed = speed;
+	}
+
+	public String getBodyColor() {
+		return bodyColor;
+	}
+
+	public void setBodyColor(String bodyColor) {
+		this.bodyColor = bodyColor;
+	}
+
 }
