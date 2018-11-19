97d9fac99a4f63bb4b316f26d5612b65be806acc
terauchi <yu.terauchi@rakus.co.jp>
2018-11-19 11:34:41 +0900

    Carクラスの修正

--- src/main/java/jp/co/sample/Class/Car.java
+++ src/main/java/jp/co/sample/Class/Car.java
@@ -12,17 +12,20 @@ public class Car {
 	/**車体の色*/
 	private String bodyColor;
 	
+	private int price;
+
 	public Car() {}
 	
-	public Car(Integer speed, String bodyColor) {
+	public Car(Integer speed, String bodyColor, int price) {
 		super();
 		this.speed = speed;
 		this.bodyColor = bodyColor;
+		this.price = price;
 	}
 
 	@Override
 	public String toString() {
-		return "Car [speed=" + speed + ", bodyColor=" + bodyColor + "]";
+		return "Car [speed=" + speed + ", bodyColor=" + bodyColor + ", price=" + price + "]";
 	}
 
 	public Integer getSpeed() {
@@ -40,6 +43,14 @@ public class Car {
 	public void setBodyColor(String bodyColor) {
 		this.bodyColor = bodyColor;
 	}
-	
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
 	
 }
