6c8bb6a66a2d11ba275739e47382629943b32821
terauchi <yu.terauchi@rakus.co.jp>
2018-11-19 10:51:23 +0900

    ドメインクラスへの情報追加

--- src/main/java/jp/co/sample/Class/Car.java
+++ src/main/java/jp/co/sample/Class/Car.java
@@ -1,5 +1,45 @@
 package jp.co.sample.Class;
 
+/**
+ * 車に関するドメイン
+ * 
+ * @author yu.terauchi
+ *
+ */
 public class Car {
+	/**スピード*/
+	private Integer speed;
+	/**車体の色*/
+	private String bodyColor;
+	
+	public Car() {}
+	
+	public Car(Integer speed, String bodyColor) {
+		super();
+		this.speed = speed;
+		this.bodyColor = bodyColor;
+	}
 
+	@Override
+	public String toString() {
+		return "Car [speed=" + speed + ", bodyColor=" + bodyColor + "]";
+	}
+
+	public Integer getSpeed() {
+		return speed;
+	}
+
+	public void setSpeed(Integer speed) {
+		this.speed = speed;
+	}
+
+	public String getBodyColor() {
+		return bodyColor;
+	}
+
+	public void setBodyColor(String bodyColor) {
+		this.bodyColor = bodyColor;
+	}
+	
+	
 }
