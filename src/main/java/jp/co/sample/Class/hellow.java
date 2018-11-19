d4a69d587afd2b223db68c0f297799cd77ace144
yuta.ikeda <yuta.ikeda@rakus.co.jp>
2018-11-19 10:50:17 +0900

    hellow 追加

---
+++ src/main/java/jp/co/sample/Class/hellow.java
@@ -0,0 +1,14 @@
+package jp.co.sample.Class;
+
+public class hellow {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		
+		
+		System.out.println("こんにちは");
+	}
+
+}
